b2d8b01033d9f2a51c3a3d8b4e43d40b90af8dab
Natalia Bezhenar <n.bezhenar@andersenlab.com>
2023-02-05 12:00:56 +0100

    update SongList

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Lyrical-GraphQL
+
 Starter project from a GraphQL course on Udemy.com
+
+query example:
+{
+songs {
+title
+id
+}
+}
+
+mutation example:
+mutation AddSong($title: String) {
+addSong(title: $title){
+id
+title
+}
+}
--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -3,6 +3,8 @@ import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 import { Link, hashHistory } from "react-router";
 
+import query from "../queries/fetchSongs";
+
 class SongCreate extends Component {
   constructor(props) {
     super(props);
@@ -12,7 +14,10 @@ class SongCreate extends Component {
   onSubmit(event) {
     event.preventDefault();
     this.props
-      .mutate({ variables: { title: this.state.title } })
+      .mutate({
+        variables: { title: this.state.title },
+        refetchQueries: [{ query }],
+      })
       .then(() => hashHistory.push("/"));
   }
 
